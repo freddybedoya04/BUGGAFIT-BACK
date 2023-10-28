3bf7e4abdcf81216d4189b83d2c6013f992675b3
Henry Acevedo <harryramirez69@gmail.com>
2023-10-27 21:03:48 -0500

    agregar El filtro de fecha en la consulta de los gastos

--- BUGGAFIT_BACK/Controllers/GastosController.cs
+++ BUGGAFIT_BACK/Controllers/GastosController.cs
@@ -1,5 +1,6 @@
 ﻿using BUGGAFIT_BACK.Catalogos;
 using BUGGAFIT_BACK.Clases;
+using BUGGAFIT_BACK.DTOs;
 using BUGGAFIT_BACK.DTOs.Response;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,11 @@ namespace BUGGAFIT_BACK.Controllers
     [ApiController]
     public class GastosController : ControllerBase
     {
-        private readonly ICatalogoGastos catalgo;
+        private readonly ICatalogoGastos catalogo;
 
         public GastosController(ICatalogoGastos catalgo)
         {
-            this.catalgo = catalgo;
+            this.catalogo = catalgo;
         }
 
         #region Metodos API
@@ -23,7 +24,7 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                var result = await catalgo.ListarGastosAsync();
+                var result = await catalogo.ListarGastosAsync();
 
                 if (result.StatusCode == 204)
                     return NoContent();
@@ -42,8 +43,27 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                var result = await catalgo.ListarGastoPorIDAsync(id);
+                var result = await catalogo.ListarGastoPorIDAsync(id);
 
+                if (result.StatusCode == 204)
+                    return NoContent();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return Problem(statusCode: 500, title: $"Error al intentar procesar la peticion.", detail: $"{ex.Message} Inner Exception: {ex.InnerException?.Message}");
+            }
+        }
+
+        // POST api/<ComprasController>
+        [HttpPost]
+        [Route("ListarGastosPorFecha")]
+        public async Task<IActionResult> ListarGastosPorFecha([FromBody] FiltrosDTO filtro)
+        {
+            try
+            {
+                var result = await catalogo.ListarGastosPorFecha(filtro);
                 if (result.StatusCode == 204)
                     return NoContent();
 
@@ -61,7 +81,7 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                var result = await catalgo.ListarMotivoGastosDeEnvioAsync();
+                var result = await catalogo.ListarMotivoGastosDeEnvioAsync();
 
                 if (result.StatusCode == 204)
                     return NoContent();
@@ -78,7 +98,7 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                var result = await catalgo.CerrarGasto(id);
+                var result = await catalogo.CerrarGasto(id);
 
                 if (result.StatusCode == 204)
                     return NoContent();
@@ -97,7 +117,7 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                return Ok(await catalgo.CrearGastoAsync(gasto));
+                return Ok(await catalogo.CrearGastoAsync(gasto));
             }
             catch (Exception ex)
             {
@@ -110,7 +130,7 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                return Ok(await catalgo.CrearGastoVentaAsync(gasto));
+                return Ok(await catalogo.CrearGastoVentaAsync(gasto));
             }
             catch (Exception ex)
             {
@@ -126,7 +146,7 @@ namespace BUGGAFIT_BACK.Controllers
                 return BadRequest(ResponseClass.Response(statusCode: 400, message: "El id no coincide."));
             try
             {
-                var result = await catalgo.ActualizarGastoAsync(gasto);
+                var result = await catalogo.ActualizarGastoAsync(gasto);
                 if (result.StatusCode == 400)
                     return NotFound(result);
 
@@ -144,7 +164,7 @@ namespace BUGGAFIT_BACK.Controllers
         {
             try
             {
-                var result = await catalgo.BorrarGastoAsync(id);
+                var result = await catalogo.BorrarGastoAsync(id);
                 if (result.StatusCode == 400)
                     return NotFound(result);
 
